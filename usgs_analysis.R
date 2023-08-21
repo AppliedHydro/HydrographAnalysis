ad09ed349956bfdeb0af2ac37f1ad4c750d02b14
Kendra Kaiser <kendrakaiser@boisestate.edu>
2023-08-21 10:57:19 -0600

    inital monthly summaries and figures

--- usgs_analysis.R
+++ usgs_analysis.R
@@ -20,7 +20,7 @@ sugar = 13311450
 usgs_sites = c(johnson, efsf, sugar)#  put all sites in one vector
 
 pCode = "00060" # USGS code for streamflow
-site_info<- whatNWISdata(sites= usgs_site, parameterCd = pCode, outputDataTypeCd ='uv') 
+site_info<- whatNWISdata(sites= usgs_sites, parameterCd = pCode, outputDataTypeCd ='uv') 
 site_info <- site_info %>% dplyr::select(site_no, station_nm, dec_lat_va, dec_long_va, alt_va, huc_cd, begin_date, end_date, count_nu)
 
 # Download data from all sites into one dataframe
@@ -49,7 +49,9 @@ mo_stats<- function(site){
   annual_flows <- q%>% group_by(wy) %>%
     summarize(min = min(Flow, na.rm=TRUE))
   
-  return(list(monthly_q, annual_flows))
+  quantiles <- quantile(q$Flow, probs =c(0.05, 0.25, 0.5, 0.75, 0.95), na.rm=TRUE) %>% round(1)
+
+  return(list(monthly_q, annual_flows, quantiles))
 }
 
 johnson_stats<-mo_stats(johnson)
@@ -58,13 +60,88 @@ ggplot(johnson_stats[[1]], aes(mo, min$min), color='r') +
   theme_bw() +
   ylab("Johnson Creek Min Monthly Streamflow (cfs)")
 
+ggplot(johnson_stats[[2]], aes(wy, min), color='r') + 
+  geom_point() +
+  theme_bw() +
+  ylab("Johnson Creek Annual Minimum Streamflow (cfs)")
+
+
 efsf_stats<-mo_stats(efsf)
 ggplot(efsf_stats[[1]], aes(mo, min$min), color='r') + 
   geom_line() +
   theme_bw() +
-  ylab("Johnson Creek Min Monthly Streamflow (cfs)")
+  ylab("EFSF Min Monthly Streamflow (cfs)")
+
+ggplot(efsf_stats[[1]], aes(mo, max$max), color='r') + 
+  geom_line() +
+  theme_bw() +
+  ylab("EFSF Max Monthly Streamflow (cfs)")
+
+ggplot(efsf_stats[[1]], aes(mo, avg), color='r') + 
+  geom_line() +
+  theme_bw() +
+  ylab("EFSF Avg Monthly Streamflow (cfs)")
+
+
 
 ggplot(efsf_stats[[2]], aes(wy, min), color='r') + 
   geom_point() +
   theme_bw() +
   ylab("EFSF above Sugar Annual Minimum Streamflow (cfs)")
+
+
+flow<- streamflow_data %>% filter(Date >= '2011-09-15' & Date <= '2022-1-15')
+jc<-flow[flow$site_no == johnson,]
+ec<- flow[flow$site_no == efsf,]
+sug<- flow[flow$site_no == sugar,]
+
+#get all the periods of record the same to compute total flow efsf+sugar and then calc proportion of efsf
+ggplot(flow, aes(Flow[site_no == johnson],Flow[site_no == efsf]))+
+  geom_point()
+
+plot(flow$Flow[flow$site_no == johnson],flow$Flow[flow$site_no == efsf])
+
+plot(flow$Date[flow$site_no == johnson], flow$Flow[flow$site_no == johnson], type='l')
+lines(flow$Date[flow$site_no == efsf], flow$Flow[flow$site_no == efsf])
+plot(flow$Date[flow$site_no == sug], flow$Flow[flow$site_no == sug])
+
+mod<-lm(flow$Flow[flow$site_no == efsf]~flow$Flow[flow$site_no == johnson])
+summary(mod)
+
+sugar_stats<-mo_stats(sugar)
+ggplot(sugar_stats[[2]], aes(wy, min), color='r') + 
+  geom_point() +
+  theme_bw() +
+  ylab("Sugar Creek Annual Minimum Streamflow (cfs)")
+
+ggplot(sugar_stats[[1]], aes(mo, max$max), color='r') + 
+  geom_point() +
+  theme_bw() +
+  ylab("Sugar Creek Monthyl Max Streamflow (cfs)")
+
+ggplot(sugar_stats[[1]], aes(mo, avg), color='r') + 
+  geom_point() +
+  theme_bw() +
+  ylab("Sugar Creek Monthyl Max Streamflow (cfs)")
+
+plot(sugar_stats[[1]])
+
+plot(flow$Date[flow$site_no == efsf], flow$Flow[flow$site_no == efsf], type='l')
+lines(flow$Date[flow$site_no == sugar], flow$Flow[flow$site_no == sugar], col='blue')
+
+total<-merge(ec, sug, by ='Date')
+total$q<- total$Flow.x+total$Flow.y
+total$efp<- total$Flow.x/total$q
+  
+plot(total$efp, type='l')
+curtailment =0.8
+for (i in 1:length(total$q)){
+  if (total$q[i] <= 25){
+    total$div[i] <- total$q[i]*curtailment
+   } else if (total$q[i] >= 25){
+   total$div[i] <- 9.6 }
+}
+
+total$ef_remaining= total$Flow.x - total$div
+
+
